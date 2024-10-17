7f6a12c5bb4fc4961752cb891c75ecf7c822ec50
Nas103 <tumelorhulani33@gmail.com>
2024-10-17 11:56:33 +0000

    msql

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,8 +1,13 @@
+-- Task 0: Create a 'users' table with a unique email constraint
+-- The table has the following attributes:
+-- id: integer, never null, auto increment, primary key
+-- email: string (255 characters), never null, unique
+-- name: string (255 characters)
+-- The script should not fail if the table already exists
 
-
--- Task 0: Create a 'users' table with a unique email constraint.
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
