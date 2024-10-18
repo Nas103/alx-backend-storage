688d991f941f389086ae50ccce927a0a66166649
Nas103 <tumelorhulani33@gmail.com>
2024-10-18 15:25:24 +0000

    sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,8 @@
 -- Task 2: Rank the origins of bands by the number of non-unique fans
 -- The result will display the origin and the total number of fans
--- The query will order the results by the number of fans in descending order
+-- The query will order the results by the number of fans in descending 
 
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
